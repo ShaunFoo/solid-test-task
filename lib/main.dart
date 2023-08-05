3a57a342c22f854b37d65ef0dafeefd8d0c8ff56
Shaun Foo <shaun.f@btigroup.io>
2023-08-05 11:37:09 +0800

    Added smooth transition to change color with animationController and animatedBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -29,42 +29,83 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Color?> _containerColorAnimation;
+  late Animation<Color?> _textColorAnimation;
+
   Color _backgroundColor = ColorUtil.generateRandomColor();
   Color _textColor = ColorUtil.generateRandomColor();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500), // Animation duration
+    );
+
+    _containerColorAnimation =
+        ColorTween(begin: _backgroundColor, end: _backgroundColor)
+            .animate(_controller);
+    _textColorAnimation =
+        ColorTween(begin: _textColor, end: _textColor).animate(_controller);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: InkWell(
-          onTap: () {
-            setState(() {
-              _backgroundColor = ColorUtil.generateRandomColor();
-              Color textColor = ColorUtil.generateRandomColor();
-
-              if (_backgroundColor != textColor) {
-                _textColor = textColor;
-              }
-            });
-          },
-          child: Container(
-            width: double.infinity,
-            color: _backgroundColor,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'HELLO THERE',
-                  style: GoogleFonts.lilitaOne(
-                      textStyle: const TextStyle(fontSize: 50),
-                      color: _textColor),
+          onTap: _changeColors,
+          child: AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return Container(
+                width: double.infinity,
+                color: _containerColorAnimation.value,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'HELLO THERE',
+                      style: GoogleFonts.lilitaOne(
+                          textStyle: const TextStyle(fontSize: 50),
+                          color: _textColorAnimation.value),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              );
+            },
           ),
         ),
       ),
     );
+  }
+
+  void _changeColors() {
+    final newBackgroundColor = ColorUtil.generateRandomColor();
+    final newTextColor = ColorUtil.generateRandomColor();
+
+    _containerColorAnimation =
+        ColorTween(begin: _backgroundColor, end: newBackgroundColor)
+            .animate(_controller);
+    _textColorAnimation =
+        ColorTween(begin: _textColor, end: newTextColor).animate(_controller);
+
+    _controller.forward(from: 0.0);
+
+    setState(() {
+      _backgroundColor = newBackgroundColor;
+      _textColor = newTextColor;
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
